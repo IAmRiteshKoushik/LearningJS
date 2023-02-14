7d2be537890483f20d1752d5845d67d48bf16957
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-14 12:01:17 +0530

    Hello World

---
+++ chapter_01/00_introduction.js
@@ -0,0 +1,10 @@
+// First Line of Code
+console.log("This program adds 1, 2 and 54")
+
+// Introductory Function
+function addThreeNumbers(a, b, c){
+    return a + b + c
+}
+
+let c = addThreeNumbers(1, 2, 54)
+console.log(c)
