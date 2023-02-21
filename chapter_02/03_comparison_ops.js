54ac6c334a4edcb30238f00c44d097007054c29d
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-21 19:44:54 +0530

    Adding Chapter-02 : 02_comparison_operators.js

---
+++ chapter_02/03_comparison_ops.js
@@ -0,0 +1,12 @@
+// Comparison operators
+let comp1 = 6;
+let comp2 = 7;
+
+console.log("comp1 == comp2", comp1 == comp2) 
+// false
+console.log("comp1 != comp2", comp1 != comp2)
+// true
+console.log("comp1 === comp2", comp1 === comp2)
+// false as the data type and the content both do not match
+console.log("comp1 !== comp2", comp1 !== comp2)
+// true as the previous case is false (NOT == )
