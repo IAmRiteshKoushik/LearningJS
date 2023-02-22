adefff8fb0064a569c0f0474d361353d89c14251
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-22 21:28:12 +0530

    Introduction to for loop

---
+++ chapter_03_loops/01_forloop.js
@@ -0,0 +1,13 @@
+// Basic for-loop
+for(let i = 0; i < 34; i++){
+    console.log(i)
+}
+
+// Program to add first n-natural numbers
+let sum = 0
+
+for (let i = 0; i < 11; i++){
+    sum += i
+
+}
+console.log("Sum of first n-natural numbers", sum)
