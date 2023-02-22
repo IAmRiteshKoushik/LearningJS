cf0e8f21f7b27dffea3ae299517fa76c738fc0e9
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-22 11:42:29 +0530

    Completed if statements

---
+++ chapter_02_operator/05_conditional_stt.js
@@ -0,0 +1,17 @@
+let age = 15
+
+if (age <= 0){
+    console.log("This is a valid age");
+}
+else if (age < 9 && age > 0){
+    console.log("You are a kid. Do not even think of driving now!")
+}
+else if (age < 18 && age >= 9){
+    console.log("You are a kid. You may drive after a few years.")
+}
+else{
+    console.log("You are over 18 year now. You can drive!");
+}
+
+// Prefer to use semi-colons inside if statements.
+  
