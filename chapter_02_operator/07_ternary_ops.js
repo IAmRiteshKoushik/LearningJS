d24574ec766b330d5c7b2762481f7ac68685ae16
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-22 11:48:38 +0530

    Adding ternary operators

---
+++ chapter_02_operator/07_ternary_ops.js
@@ -0,0 +1,8 @@
+// Ternary Operator
+var isMember = true
+var price = isMember ? '2 rupees' : '4 rupees'
+// Either prints true-value or false-value
+// Avoid creating nested ternary operators
+// They are to be used for only single line
+// True or False values...
+console.log(price)
