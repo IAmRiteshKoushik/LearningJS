efe3ab5732391f2f69629c8e653bc8eb743486f6
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-23 00:34:32 +0530

    Completed Switch Statements

---
+++ chapter_02_operator/06_switch.js
@@ -0,0 +1,33 @@
+// Switch statements in JavaScript
+let day = 7;
+
+switch(day){
+    case 1:
+        text = "Today is Monday";
+        break;
+    case 2:
+        text = "Today is Tuesday";
+        break;
+    case 3:
+        text = "Today is Wednesday";
+        break;
+    case 4:
+        text = "Today is Thursday";
+        break;
+    case 5:
+        text = "Today is Friday";
+        break;
+    case 6:
+        text = "Today is Saturday";
+        break;
+    case 7:
+        text = "Today is Sunday";
+        break;
+}
+
+console.log(text)
+
+// Also, variables declared without any of the
+// - var, const and let automatically qualify
+// as GLOBAL.
+
