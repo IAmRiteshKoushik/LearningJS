6fba37b5530a77d488a9a2614d8d90c8cdf4d007
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-22 11:32:11 +0530

    Completed Prompts and Alerts

---
+++ chapter_02_operator/04_prompts_alerts.js
@@ -0,0 +1,19 @@
+// "Prompts" and "alerts" are not defined on the 
+// server side. Hence, they would not be 
+// giving any outputs and would generate
+// error messages. 
+
+// let a = prompt("Hey what is your name ?")
+// alert("Hello folks")
+
+// The default datatype of prompt is string.
+// This is similar to python input. 
+
+// --------------------------------------
+
+
+// Type Casting
+let a = 45
+a = Number.parseInt(a) // Converting the string 
+    // to number
+console.log(a)
