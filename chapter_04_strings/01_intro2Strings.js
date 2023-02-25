08a41b849572c3aaf5c091cea77de735cd8cd747
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-25 20:02:33 +0530

    Got introduced to strings

---
+++ chapter_04_strings/01_intro2Strings.js
@@ -0,0 +1,16 @@
+let name = 'Harry'
+console.log(name);
+
+// There is an issue with indexing if you do not 
+// add semi-colons in javascript
+// This issue does not happen in repl.it but 
+// does happen when you try to run in local 
+// machine (or) node.js
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+
+// Printing the final length
+console.log(name.length);
