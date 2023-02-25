09336894063be308238c4fcd7f92ac7647b13328
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-25 20:02:54 +0530

    Template characters

---
+++ chapter_04_strings/02_modernjs.js
@@ -0,0 +1,11 @@
+// Strings in modern javascript - Template Literal
+let boy1 = "Nikhil"
+let boy2 = "Pramod"
+
+let sentence = `${boy1} is a friend of ${boy2}.`
+console.log(sentence)
+
+// Escpae sequence characters -- All backslash
+// remain same from Python
+let fruit = 'This is Sitesh\'s fruit'
+console.log(fruit)
