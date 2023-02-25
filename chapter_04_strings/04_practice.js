9c9230bab8ce0675d737addb6812935db1f6e2e3
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-25 21:11:04 +0530

    Completed practice-set of strings

---
+++ chapter_04_strings/04_practice.js
@@ -0,0 +1,24 @@
+// Question 01
+console.log("har\"".length)   // Length = 4
+
+
+// Question 02
+let variable = "hello world"
+// Returns boolean values
+console.log(variable.startsWith('hello'))
+console.log(variable.endsWith('ld'))
+console.log(variable.includes('o w'))
+
+// Question 03
+console.log(variable.toLowerCase())
+
+// Question 04
+let str = "Please give Rs1000"
+console.log(str.slice(str.length - 4, str.length))
+
+// Question 05
+let str1 = "hello"
+str1[4] = "g"
+console.log(str1)
+// Not possible as the string is immutable
+
