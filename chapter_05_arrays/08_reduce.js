c2fd2207f9b44ac6825e6db533e791db492e6fb9
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-05 11:12:51 +0530

    Added 08_reduce.js

---
+++ chapter_05_arrays/08_reduce.js
@@ -0,0 +1,12 @@
+// Reduce function in JavaScript
+const arr = [1, 2, 3, 4, 5, 6, 7]
+let arr2 = arr.reduce((h1, h2) => {
+    return h1 + h2;
+})
+console.log(arr2)
+
+// Reduce function is used to take in values
+// from an array, compute them and then 
+// store them in a variable. 
+
+
