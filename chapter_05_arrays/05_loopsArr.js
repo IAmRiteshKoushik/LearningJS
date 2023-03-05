61cc0ed6af11eb20bf9da0f55f948c0480ee01b0
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-05 09:06:23 +0530

    Add forEach loop 05_loopsArr.js

---
+++ chapter_05_arrays/05_loopsArr.js
@@ -0,0 +1,10 @@
+// This is actually a function disguised as a loop
+const a = [11, 22, 33]
+
+// (element, index(optional), array(optional)) 
+a.forEach((element, index) => {
+    console.log(index, element);
+});
+
+// For more details, check w3schools.com
+
