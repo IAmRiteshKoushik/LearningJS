84b26592e216902d54704e9640569197454f193c
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-05 09:48:08 +0530

    Add map method

---
+++ chapter_05_arrays/06_map.js
@@ -0,0 +1,10 @@
+let arr = [45, 23, 21]
+
+// Map returns a new array (basically mapping values to array indexes)
+let a = arr.map((value) => {
+    return value + 1;
+})
+
+console.log(a)
+// forEach method does not return a new array
+
