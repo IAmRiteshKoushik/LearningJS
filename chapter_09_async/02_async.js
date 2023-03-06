809dae4a452b22ebf275383fdf1bca1b763fe43a
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-06 21:08:20 +0530

    Async JavaScript:

---
+++ chapter_09_async/02_async.js
@@ -0,0 +1,14 @@
+// Asynchronous JavaScript
+
+
+console.log('I');
+console.log('want');
+
+// Making an asynchronous call -- Add 4s delay
+setTimeout(() => {
+    console.log("ice cream");
+}, 4000);
+
+console.log('to');
+console.log('eat');
+
